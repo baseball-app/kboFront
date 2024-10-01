e143d58e2a5ff1fc78d83eb4e2742fb45c28ec44
JiHoon <gkwlgnd@naver.com>
2024-10-01 21:48:27 +0900

    first-commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,68 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView, Image, useWindowDimensions } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-// import LandingLogo from '../../assets/images/landing-logo@2x.png';
+import {
+  StyleSheet,
+  Platform,
+  ScrollView,
+  Image,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import Calendar from "@/components/Calendar";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FriendList from "@/components/FrendList";
+import GameInfoCard from "@/components/GameInfoCard";
+import { useStore } from "@/store/useStore";
+import { useEffect } from "react";
 
 export default function HomeScreen() {
-  const { width: windowWidth } = useWindowDimensions();
+  const count = useStore((state) => state.count);
+  const increase = useStore((state) => state.increase);
 
-  // Calculate the maximum image width based on the screen width
-  const maxImageWidth = windowWidth * 0.40; // 20% of screen width
-
+  useEffect(() => {
+    console.log("count", count);
+  }, [count]);
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Image 
-          source={require('../../assets/images/landing-logo.png')} 
-          style={[styles.icon, { width: maxImageWidth }]}
-          resizeMode="contain"
-        />
-        <Text style={styles.title}>오늘의 야구</Text>
-        <Text style={styles.subtitle}>반가워요! 오늘의 야구와 함께{'\n'}내가 응원하는 구단을 기록해보세요</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>시작하기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginLink}>
-          <Text style={styles.loginText}>이미 회원이신가요? <Text style={styles.loginBold}>로그인</Text></Text>
-        </TouchableOpacity>
-      </View>
+      <FriendList />
+      <ScrollView style={styles.scollContainer}>
+        <View style={styles.tabMenu}>
+          <TouchableOpacity style={styles.tabMenuButton}>
+            <View style={styles.arrowImgBox}>
+              <Image
+                source={require("@/assets/icons/left-arrow.png")}
+                resizeMode="contain"
+              />
+            </View>
+            <Text style={styles.dayText}>어제의 야구</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabMenuButton}>
+            <Text style={styles.todayText}>오늘의 야구</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabMenuButton}>
+            <Text style={styles.dayText}>내일의 야구</Text>
+            <View style={styles.arrowImgBox}>
+              <Image
+                source={require("@/assets/icons/right-arrow.png")}
+                resizeMode="contain"
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <GameInfoCard matchSchedule="7월 16일(목) 15:00 ・ 수원" />
+        <Calendar />
+        <View style={styles.writeButtonBox}>
+          <TouchableOpacity
+            style={styles.writeScheduleButton}
+            onPress={increase}
+          >
+            <Image
+              source={require("@/assets/icons/write.png")}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      {/* <Footer /> */}
     </SafeAreaView>
   );
 }
@@ -33,77 +70,54 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#fff",
   },
-  content: {
+  scollContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 20,
+    backgroundColor: "#fffcf3",
+    padding: 24,
   },
-  icon: {
-    // marginBottom: 20,
-    height: undefined, // Allow height to adjust based on the image's aspect ratio
-    aspectRatio: 1, // This should be adjusted to match your image's actual aspect ratio
-  },
-  calendarIcon: {
-    width: 60,
-    height: 60,
-    borderWidth: 2,
-    borderColor: '#000',
-    borderRadius: 10,
-    overflow: 'hidden',
-  },
-  calendarTop: {
-    height: 15,
-    backgroundColor: '#4A90E2',
-  },
-  calendarBody: {
+  tabMenu: {
     flex: 1,
-    backgroundColor: '#E6E6E6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
-  baseball: {
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    backgroundColor: '#FFFFFF',
-    borderWidth: 2,
-    borderColor: '#FF0000',
+  tabMenuButton: {
+    flexDirection: "row",
+    alignItems: "center",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subtitle: {
+  todayText: {
     fontSize: 16,
-    textAlign: 'center',
-    marginBottom: 30,
-    color: '#666',
+    color: "#171716",
+    fontWeight: 600,
+    lineHeight: 22.4,
   },
-  button: {
-    backgroundColor: '#4A90E2',
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 8,
-    marginBottom: 20,
+  dayText: {
+    fontSize: 12,
+    color: "#95938B",
+    fontWeight: 600,
+    textAlignVertical: "center",
   },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontWeight: 'bold',
+  arrowImgBox: {
+    height: 20,
+    width: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  loginLink: {
-    marginTop: 10,
+  writeButtonBox: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-end",
   },
-  loginText: {
-    fontSize: 14,
-    color: '#666',
-  },
-  loginBold: {
-    fontWeight: 'bold',
-    color: '#000',
+  writeScheduleButton: {
+    width: 48,
+    height: 48,
+    backgroundColor: "#353430",
+    borderRadius: 40,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
