718e1fd0eb2711828734aa954a555800ac771a85
Jong1co <narira0922@gmail.com>
2025-01-17 18:08:57 +0900

    feat: userJoinSlice 데이터 추가
    - 회원가입에 필요한 code 추가
    - 약관동의 데이터 (consent) -> userJoinSlice로 포함

--- app/auth/component/KaKaoLoginModal.tsx
+++ app/auth/component/KaKaoLoginModal.tsx
@@ -1,105 +1,93 @@
-import { Modal } from "react-native";
-import WebView from "react-native-webview";
-import { SafeAreaView, TouchableOpacity, Text, StyleSheet } from "react-native";
-import { router } from "expo-router";
-import { useRef } from "react";
+import {Modal} from 'react-native'
+import WebView from 'react-native-webview'
+import {SafeAreaView, TouchableOpacity, Text, StyleSheet} from 'react-native'
+import {useRef} from 'react'
+import useUserJoin from '@/hooks/auth/useUserJoin'
 
 interface KaKaoLoginModalProps {
-  showWebView: boolean;
-  setShowWebView: (showWebView: boolean) => void;
-  isKakaoLoginPage: boolean;
-  setIsKakaoLoginPage: (isKakaoLoginPage: boolean) => void;
-  //   KAKAO_AUTH_URL: string;
-  onLoginSuccess: (code: string) => void;
+    showWebView: boolean
+    setShowWebView: (showWebView: boolean) => void
+    isKakaoLoginPage: boolean
+    setIsKakaoLoginPage: (isKakaoLoginPage: boolean) => void
+    //   KAKAO_AUTH_URL: string;
+    onLoginSuccess: (code: string) => void
 }
 
-const KAKAO_CLIENT_ID = process.env.EXPO_PUBLIC_KAKAO_LOGIN_CLIENT_ID;
-const KAKAO_REDIRECT_URI = process.env.EXPO_PUBLIC_KAKAO_LOGIN_REDIRECT_URI;
+const KAKAO_CLIENT_ID = process.env.EXPO_PUBLIC_KAKAO_LOGIN_CLIENT_ID
+const KAKAO_REDIRECT_URI = process.env.EXPO_PUBLIC_KAKAO_LOGIN_REDIRECT_URI
 const KAKAO_AUTH_URL = [
-  "https://kauth.kakao.com/oauth/authorize?",
-  `client_id=${KAKAO_CLIENT_ID}`,
-  `redirect_uri=${KAKAO_REDIRECT_URI}`,
-  "response_type=code",
-].join("&");
+    'https://kauth.kakao.com/oauth/authorize?',
+    `client_id=${KAKAO_CLIENT_ID}`,
+    `redirect_uri=${KAKAO_REDIRECT_URI}`,
+    'response_type=code',
+].join('&')
+
+type NavState = {
+    canGoBack: boolean // false
+    canGoForward: boolean // false
+    loading: boolean // true
+    lockIdentifier: number // 0
+    mainDocumentURL: string // ''
+    navigationType: string // 'formsubmit'
+    target: number // 92
+    title: string // '카카오계정으로 로그인'
+    url: string // 'http://localhost:8000/auths/kakao/callback?code=ltY5k0uhqPzqy5sQ4NyCDFMyTCrs03HLsICn9pTkResnHMfUjasmWgAAAAQKPXKYAAABlHLXYHgq3eF1vjqPRg'
+}
 
 const KaKaoLoginModal = ({
-  showWebView,
-  setShowWebView,
-  isKakaoLoginPage,
-  setIsKakaoLoginPage,
-  //   KAKAO_AUTH_URL,
-  onLoginSuccess,
+    showWebView,
+    setShowWebView,
+    isKakaoLoginPage,
+    setIsKakaoLoginPage,
+    //   KAKAO_AUTH_URL,
+    onLoginSuccess,
 }: KaKaoLoginModalProps) => {
-  const webViewRef = useRef(null);
+    const {startSignUpProcessWithCode} = useUserJoin()
 
-  return (
-    <Modal
-      visible={showWebView}
-      onRequestClose={() => !isKakaoLoginPage && setShowWebView(false)}
-      animationType="slide"
-    >
-      <SafeAreaView style={styles.webViewContainer}>
-        <WebView
-          source={{ uri: KAKAO_AUTH_URL }}
-          onNavigationStateChange={(navState) => {
-            console.log("navState.url == ", navState.url);
-            console.log("navState :: ", navState);
+    const webViewRef = useRef(null)
 
-            console.log(
-              "*******navState.url.startsWith(KAKAO_AUTH_URL)",
-              navState.url.startsWith(KAKAO_REDIRECT_URI || "")
-            );
+    return (
+        <Modal
+            visible={showWebView}
+            onRequestClose={() => !isKakaoLoginPage && setShowWebView(false)}
+            animationType="slide">
+            <SafeAreaView style={styles.webViewContainer}>
+                <WebView
+                    source={{uri: KAKAO_AUTH_URL}}
+                    onNavigationStateChange={navState => {
+                        const code = (navState as NavState).url?.split('code=')[1]?.split('&')[0]
 
-            console.log(
-              "navState.url?.split('code=')[1]?.split('&')[0]",
-              navState.url?.split("code=")[1]?.split("&")[0]
-            );
-
-            const code = navState.url?.split("code=")[1]?.split("&")[0];
-
-            console.log("code:*********************", code);
-            if (code) {
-              console.log("code:", code);
-              // TODO: 여기에서 code -> register api 호출
-              // 그럼 로그인 vs 회원가입은 어떻게 구분하지 ?
-              setShowWebView(false);
-
-              // 로그인 회원가입 구분해서 분기처리
-              router.navigate("/auth/term-of-service");
-            }
-
-            if (navState.title === "Login with Kakao Account") {
-              setIsKakaoLoginPage(true);
-            } else {
-              setIsKakaoLoginPage(false);
-            }
-          }}
-        />
-        {!isKakaoLoginPage && (
-          <TouchableOpacity
-            style={styles.closeButton}
-            onPress={() => setShowWebView(false)}
-          >
-            <Text>Close</Text>
-          </TouchableOpacity>
-        )}
-      </SafeAreaView>
-    </Modal>
-  );
-};
+                        if (code) {
+                            // TODO: 여기에서 code -> register api 호출
+                            // 그럼 로그인 vs 회원가입은 어떻게 구분하지 ?
+                            setShowWebView(false)
+                            // 로그인 회원가입 구분해서 분기처리
+                            startSignUpProcessWithCode(code)
+                        }
+                    }}
+                />
+                {!isKakaoLoginPage && (
+                    <TouchableOpacity style={styles.closeButton} onPress={() => setShowWebView(false)}>
+                        <Text>Close</Text>
+                    </TouchableOpacity>
+                )}
+            </SafeAreaView>
+        </Modal>
+    )
+}
 
 const styles = StyleSheet.create({
-  webViewContainer: {
-    flex: 1,
-  },
-  closeButton: {
-    position: "absolute",
-    top: 50,
-    right: 20,
-    backgroundColor: "#fff",
-    padding: 10,
-    borderRadius: 5,
-  },
-});
+    webViewContainer: {
+        flex: 1,
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 50,
+        right: 20,
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 5,
+    },
+})
 
-export default KaKaoLoginModal;
+export default KaKaoLoginModal
