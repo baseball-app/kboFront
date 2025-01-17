76b959d10ec7ed578997780fcc03820ef6dd1c21
Jong1co <narira0922@gmail.com>
2025-01-17 19:24:23 +0900

    feat: 로그아웃 추가

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -1,7 +1,8 @@
-import {useMemo} from 'react'
+import {useEffect, useMemo} from 'react'
 import {useMMKVObject} from 'react-native-mmkv'
-import {MmkvStoreKeys} from "@/store/mmkv-store/constants";
-
+import {MmkvStoreKeys} from '@/store/mmkv-store/constants'
+import ApiClient from '@/api'
+import {useRouter} from 'expo-router'
 
 export type TUser = {
     accessToken: string
@@ -11,10 +12,52 @@ export type TUser = {
 export const useLogin = () => {
     const [user, setUser] = useMMKVObject<TUser>(MmkvStoreKeys.USER_LOGIN)
     const isLogined = useMemo(() => user?.accessToken && user.accessToken.length > 0, [user?.accessToken])
+    const router = useRouter()
+
+    const logout = () => {
+        setUser(undefined)
+        // api 호출 /auths/token/revoke/
+        router.navigate('/auth/login')
+    }
+
+    // TODO: api 확정되면 구현 예정
+    // const refreshAccessToken = async () => {
+    //     if (!user?.refreshToken) return
+
+    //     // const response = await ApiClient.post('/auths/kakao/token', {
+    //     //     refreshToken: user.refreshToken,
+    //     // })
+
+    //     //
+    //     // user?.refreshToken
+
+    //     // setUser(prev => {
+    //     //     return {
+    //     //         accessToken: 'new access',
+    //     //         refreshToken: user.refreshToken,
+    //     //     }
+    //     // })
+    // }
+
+    // const {data} = useQuery({
+    //     queryKey: ['user'],
+    //     queryFn: () => {},
+    //     staleTime: 1000 * 60 * 10,
+    //     enable: Boolean(user?.refreshToken),
+    // })
+
+    // useEffect(() => {
+
+    //     refreshAccessToken()
+
+    //     // refresh 토큰으로 발급
+    // }, [data])
+
     return {
         user,
         isLogined,
         setUser,
+        logout,
     }
 }
 
