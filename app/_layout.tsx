6c9cb17dfddff344db1a936fb2635f101ea4355a
Jong1co <narira0922@gmail.com>
2025-01-24 17:00:44 +0900

    add: tanstack-query 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,44 @@
 import 'react-native-gesture-handler'
-import "react-native-reanimated";
-import { useEffect, useRef } from "react";
-import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
+import 'react-native-reanimated'
+import {useEffect, useRef} from 'react'
+import {Stack} from 'expo-router'
+import {useFonts} from 'expo-font'
+
+import * as SplashScreen from 'expo-splash-screen'
 
 import {enableFreeze, enableScreens} from 'react-native-screens'
+import QueryProvider from '@/components/provider/QueryProvider'
 
 enableScreens(false)
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  // const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
+    // const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    })
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync()
+        }
+    }, [loaded])
+
+    if (!loaded) {
+        return null
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <Stack>
-      <Stack.Screen name="auth" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="my" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    return (
+        <QueryProvider>
+            <Stack>
+                <Stack.Screen name="auth" options={{headerShown: false}} />
+                <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+                <Stack.Screen name="my" options={{headerShown: false}} />
+                <Stack.Screen name="+not-found" />
+                <Stack.Screen name="index" />
+            </Stack>
+        </QueryProvider>
+    )
 }
-
