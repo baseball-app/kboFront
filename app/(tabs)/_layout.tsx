4cc54533f163495a3e5233b086e97fc76d3eaadd
Jong1co <narira0922@gmail.com>
2025-01-25 22:33:59 +0900

    feat: footer 활성 이미지 추가 및 리팩터링

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,30 +1,53 @@
-import { Tabs } from "expo-router";
-import React from "react";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import Footer from "@/components/layout/Footer";
+import {Tabs} from 'expo-router'
+import React from 'react'
+import {useColorScheme} from '@/hooks/useColorScheme'
+import Footer from '@/components/layout/Footer'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme()
 
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: { display: "none" },
-      }}
-      tabBar={() => <Footer />}
-    >
-      {/* 캘린더 탭 화면 */}
-      <Tabs.Screen name="index" />
-      {/* 경기 일정 탭 화면 */}
-      <Tabs.Screen name="match" />
-      {/* 티켓 박스 탭 화면 */}
-      <Tabs.Screen name="ticket" />
-      {/* 알림 탭 화면 */}
-      <Tabs.Screen name="alarm" />
-      {/* 마이 페이지 탭 화면 */}
-      <Tabs.Screen name="my" />
-
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: true,
+                tabBarStyle: {display: 'none'},
+            }}
+            tabBar={() => <Footer />}>
+            {/* 캘린더 탭 화면 */}
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                }}
+            />
+            {/* 경기 일정 탭 화면 */}
+            <Tabs.Screen
+                name="match"
+                options={{
+                    headerShown: false,
+                }}
+            />
+            {/* 티켓 박스 탭 화면 */}
+            <Tabs.Screen
+                name="ticket"
+                options={{
+                    headerShown: false,
+                }}
+            />
+            {/* 알림 탭 화면 */}
+            <Tabs.Screen
+                name="alarm"
+                options={{
+                    title: '알림',
+                }}
+            />
+            {/* 마이 페이지 탭 화면 */}
+            <Tabs.Screen
+                name="my"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Tabs>
+    )
 }
