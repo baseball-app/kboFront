88b90b8121613e3ba00982f0037040836e8de92e
doyunnn <73818291+doyunnn@users.noreply.github.com>
2025-01-27 11:00:05 +0900

    fix:경기 일정 기본 UI 수정 (#19)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,29 +1,32 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { useColorScheme } from "@/hooks/useColorScheme";
+import {BottomTabNavigationOptions} from "@react-navigation/bottom-tabs";
+
 import Footer from "@/components/layout/Footer";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+
+    const tabScreenOptions: BottomTabNavigationOptions = {
+        tabBarHideOnKeyboard: true,
+        headerShadowVisible:false,
+        tabBarStyle: { display: "none" },
+    }
 
   return (
     <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: { display: "none" },
-      }}
+      screenOptions={tabScreenOptions}
       tabBar={() => <Footer />}
     >
       {/* 캘린더 탭 화면 */}
-      <Tabs.Screen name="index" />
+      <Tabs.Screen name="index" options={{headerShown: false}}/>
       {/* 경기 일정 탭 화면 */}
-      <Tabs.Screen name="match" />
+      <Tabs.Screen name="match" options={{title: '경기일정', }} />
       {/* 티켓 박스 탭 화면 */}
-      <Tabs.Screen name="ticket" />
+      <Tabs.Screen name="ticket" options={{title: '나의 티켓박스'}}/>
       {/* 알림 탭 화면 */}
-      <Tabs.Screen name="alarm" />
+      <Tabs.Screen name="alarm" options={{title: '알림'}}/>
       {/* 마이 페이지 탭 화면 */}
-      <Tabs.Screen name="my" />
+      <Tabs.Screen name="my" options={{headerShown:false}}/>
 
     </Tabs>
   );
