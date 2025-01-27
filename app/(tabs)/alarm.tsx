4379c829e344f57194583d7356a4783b0855a38c
Jong1co <narira0922@gmail.com>
2025-01-27 18:39:32 +0900

    feat: 알림페이지 생성

--- app/(tabs)/alarm.tsx
+++ app/(tabs)/alarm.tsx
@@ -1,29 +1,65 @@
-import { StyleSheet, Image, Platform, View, Text } from "react-native";
+import {StyleSheet, View, Text, FlatList, SafeAreaView, ScrollView} from 'react-native'
 
-import { Collapsible } from "@/components/Collapsible";
-import { ExternalLink } from "@/components/ExternalLink";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import useNotification from '@/hooks/notification/useNotification'
+import NotificationCard from '@/components/alarm/NotificationCard'
+import {format} from 'date-fns'
+import EmptyNotificationView from '@/components/alarm/EmptyNotificationView'
 
 const AlarmScreen = () => {
-  return (
-   <View>
-    <Text>알림</Text>
-   </View>
-  );
-};
-export default AlarmScreen;
+    const {notificationList, onClickNotification, fetchNextPage} = useNotification()
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.headerTitleBox}>
+                <Text style={styles.headerTitle}>알림</Text>
+            </View>
+            <ScrollView style={styles.wrapper}>
+                <FlatList
+                    data={notificationList} //  || notificationList
+                    keyExtractor={_ => String(_.id)}
+                    ListEmptyComponent={<EmptyNotificationView />}
+                    ListHeaderComponent={<View style={{height: 24}} />}
+                    ListFooterComponent={<View style={{height: 24}} />}
+                    onEndReached={fetchNextPage}
+                    onEndReachedThreshold={0.6}
+                    ItemSeparatorComponent={() => <View style={{height: 12}} />}
+                    renderItem={({item}) => (
+                        <View style={{marginInline: 24}}>
+                            <NotificationCard
+                                type={item.type}
+                                userName={item.user_info.nickname}
+                                isRead={item.is_read}
+                                date={format(item.created_at, 'YY.MM.DD')}
+                                onClick={() => onClickNotification(item)}
+                            />
+                        </View>
+                    )}
+                />
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+export default AlarmScreen
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    wrapper: {
+        flex: 1,
+        backgroundColor: '#F3F2EE',
+    },
+    headerTitleBox: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        paddingVertical: 24,
+    },
+    headerTitle: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#171716',
+    },
+})
