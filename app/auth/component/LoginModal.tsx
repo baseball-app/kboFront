10ecace50dc3a9826b87cc8adb9b237a0c0cfc58
Jong1co <narira0922@gmail.com>
2025-03-08 09:05:56 +0900

    fix: 회원가입 시, 회원가입 플로우를 거치지 않고, 바로 로그인 되는 이슈 수정

--- app/auth/component/LoginModal.tsx
+++ app/auth/component/LoginModal.tsx
@@ -1,67 +1,73 @@
 import {Modal} from 'react-native'
 import WebView from 'react-native-webview'
 import {SafeAreaView, StyleSheet} from 'react-native'
+import {useRef} from 'react'
 
 interface LoginModalProps {
-    showWebView: boolean
-    onClose: () => void
-    url: string
-    onLoginSuccess: (code: string) => void
+  showWebView: boolean
+  onClose: () => void
+  url: string
+  onLoginSuccess: (code: string) => void
 }
 
 export type NavState = {
-    canGoBack: boolean // false
-    canGoForward: boolean // false
-    loading: boolean // true
-    lockIdentifier: number // 0
-    mainDocumentURL: string // ''
-    navigationType: string // 'formsubmit'
-    target: number // 92
-    title: string // '카카오계정으로 로그인'
-    url: string // 'http://localhost:8000/auths/kakao/callback?code=ltY5k0uhqPzqy5sQ4NyCDFMyTCrs03HLsICn9pTkResnHMfUjasmWgAAAAQKPXKYAAABlHLXYHgq3eF1vjqPRg'
+  canGoBack: boolean // false
+  canGoForward: boolean // false
+  loading: boolean // true
+  lockIdentifier: number // 0
+  mainDocumentURL: string // ''
+  navigationType: string // 'formsubmit'
+  target: number // 92
+  title: string // '카카오계정으로 로그인'
+  url: string // 'http://localhost:8000/auths/kakao/callback?code=ltY5k0uhqPzqy5sQ4NyCDFMyTCrs03HLsICn9pTkResnHMfUjasmWgAAAAQKPXKYAAABlHLXYHgq3eF1vjqPRg'
 }
 
 const LoginModal = ({
-    showWebView,
-    onClose,
-    onLoginSuccess,
-    url, //
+  showWebView,
+  onClose,
+  onLoginSuccess,
+  url, //
 }: LoginModalProps) => {
-    return (
-        <Modal
-            visible={showWebView}
-            onRequestClose={onClose}
-            animationType="slide" //
-        >
-            <SafeAreaView style={styles.webViewContainer}>
-                <WebView
-                    source={{uri: url}}
-                    onNavigationStateChange={navState => {
-                        // onNavigationStateChange 함수가 loading 중일 때와 아닐 때 두번 호출되어 에러 발생하는 현상 방지
-                        if (navState.loading) return
+  const isAlreadyRequest = useRef(false)
 
-                        const code = (navState as NavState).url?.split('code=')[1]?.split('&')[0]
+  return (
+    <Modal
+      visible={showWebView}
+      onRequestClose={onClose}
+      animationType="slide" //
+    >
+      <SafeAreaView style={styles.webViewContainer}>
+        <WebView
+          source={{uri: url}}
+          onNavigationStateChange={navState => {
+            // onNavigationStateChange 함수가 loading 중일 때와 아닐 때 두번 호출되어 에러 발생하는 현상 방지
+            if (navState.loading || isAlreadyRequest.current) return
 
-                        if (code) onLoginSuccess(code)
-                    }}
-                />
-            </SafeAreaView>
-        </Modal>
-    )
+            const code = (navState as NavState).url?.split('code=')[1]?.split('&')[0]
+
+            if (code) {
+              isAlreadyRequest.current = true
+              onLoginSuccess(code)
+            }
+          }}
+        />
+      </SafeAreaView>
+    </Modal>
+  )
 }
 
 const styles = StyleSheet.create({
-    webViewContainer: {
-        flex: 1,
-    },
-    closeButton: {
-        position: 'absolute',
-        top: 50,
-        right: 20,
-        backgroundColor: '#fff',
-        padding: 10,
-        borderRadius: 5,
-    },
+  webViewContainer: {
+    flex: 1,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 50,
+    right: 20,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+  },
 })
 
 export default LoginModal
