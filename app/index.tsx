bef03cffb59dbf2ee5ca73cf4471ea5e33104735
Jong1co <narira0922@gmail.com>
2025-07-06 22:29:32 +0900

    build: v1.1.0-60

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,8 @@ import {SplashScreen, useRouter} from 'expo-router'
 import ApiClient from '@/api'
 import {Profile} from '@/hooks/my/useProfile'
 import {useEffectOnce} from '@/hooks/useEffectOnce'
+import VersionCheck from 'react-native-version-check'
+import {Alert, Linking} from 'react-native'
 
 export default function Index() {
   const {refreshAccessToken} = useLogin()
@@ -24,7 +26,28 @@ export default function Index() {
     }
   }
 
-  useEffectOnce(() => checkIsLogined())
+  useEffectOnce(() => {
+    const checkVersion = async () => {
+      try {
+        const result = await VersionCheck.needUpdate({depth: 2, forceUpdate: true})
+        if (result.isNeeded) {
+          Alert.alert('버전 업데이트 필요', '최신 버전으로 업데이트 해주세요.', [
+            {
+              text: '업데이트',
+              style: 'default',
+              onPress: () => Linking.openURL(result.storeUrl),
+            },
+          ])
+        } else {
+          checkIsLogined()
+        }
+      } catch (e) {
+        checkIsLogined()
+      }
+    }
+
+    checkVersion()
+  })
 
   return null
 }
