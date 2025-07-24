0c64cc40cb129a2fec7ca34647ba5acbcc61ba03
Jong1co <narira0922@gmail.com>
2025-07-24 21:33:09 +0900

    fix: 친구 캘린더 조회되지 않는 이슈 수정

--- components/home/Calendar/CalendarContainer.tsx
+++ components/home/Calendar/CalendarContainer.tsx
@@ -218,7 +218,7 @@ const CalendarContainer = ({targetId}: Props) => {
         />
       )
     },
-    [hideCalendar],
+    [hideCalendar, targetId],
   )
 
   const initialScrollIndex = useMemo(() => {
