cc196d1f56133495e1947f503901493664649fc0
Jong1co <narira0922@gmail.com>
2025-09-10 23:12:53 +0900

    refactor: emoji 추가

--- entities/match/index.ts
+++ entities/match/index.ts
@@ -7,6 +7,10 @@ export {
   MatchCalendarTitle,
   TeamTag,
   TicketCard,
+  WinEmoji,
+  LoseEmoji,
+  DrawEmoji,
+  CancelEmoji,
 } from './ui'
 export type {Team, TeamWithInfo, Match} from './types'
 export {useTeam, useMatch} from './model'
