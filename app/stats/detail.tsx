57d04c66ce603a2157239342a29ed2df4d48228f
Jong1co <narira0922@gmail.com>
2026-02-04 20:23:55 +0900

    fix: stats detail scroll 안 되는 이슈 수정

--- app/stats/detail.tsx
+++ app/stats/detail.tsx
@@ -8,6 +8,7 @@ import {
   TeamStatsDetail,
 } from '@/widgets/stats-detail/ui';
 import React from 'react';
+import {ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function StatsDetailScreen() {
@@ -16,12 +17,14 @@ export default function StatsDetailScreen() {
 
   return (
     <SafeAreaView>
-      <Header hasBackButton variants="transparent" />
-      {query.type === StatsDetailType.OPPONENT && <TeamStatsDetail parameter_id={query.parameter_id} />}
-      {query.type === StatsDetailType.BALLPARK && <StadiumStatsDetail parameter_id={query.parameter_id} />}
-      {query.type === StatsDetailType.HOME && <HomeStatsDetail is_homeballpark />}
-      {query.type === StatsDetailType.AWAY && <AwayStatsDetail is_homeballpark={false} />}
-      {query.type === StatsDetailType.MY_HOME && <TeamStatsByHomeDetail parameter_id={query.parameter_id} />}
+      <ScrollView contentContainerStyle={{paddingBottom: 24}}>
+        <Header hasBackButton variants="transparent" />
+        {query.type === StatsDetailType.OPPONENT && <TeamStatsDetail parameter_id={query.parameter_id} />}
+        {query.type === StatsDetailType.BALLPARK && <StadiumStatsDetail parameter_id={query.parameter_id} />}
+        {query.type === StatsDetailType.HOME && <HomeStatsDetail is_homeballpark />}
+        {query.type === StatsDetailType.AWAY && <AwayStatsDetail is_homeballpark={false} />}
+        {query.type === StatsDetailType.MY_HOME && <TeamStatsByHomeDetail parameter_id={query.parameter_id} />}
+      </ScrollView>
     </SafeAreaView>
   );
 }
