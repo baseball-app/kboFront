825b89dbf7a01937d58ec437dd4c435b5557ff96
Jong1co <narira0922@gmail.com>
2026-03-07 16:07:19 +0900

    test: EventRacker.test.tsx 파일 테스트케이스 수정
    - time, my team 추가

--- analytics/EventTracker.test.tsx
+++ analytics/EventTracker.test.tsx
@@ -4,6 +4,25 @@ import {Pressable} from '@/shared';
 
 import * as func from './func';
 import * as router from 'expo-router';
+
+jest.mock('@/entities/user', () => ({
+  useProfile: () => ({
+    profile: {my_team: {name: 'LG'}},
+  }),
+}));
+
+const FIXED_TIME = '2026-03-07 12:00:00';
+jest.mock('dayjs', () => {
+  const original = jest.requireActual('dayjs');
+  const mockDayjs = (...args: any[]) => {
+    const instance = (original as any)(...args);
+    instance.format = jest.fn(() => FIXED_TIME);
+    return instance;
+  };
+  Object.assign(mockDayjs, original);
+  return {__esModule: true, default: mockDayjs};
+});
+
 describe('EventTracker', () => {
   let logEventSpy: jest.SpyInstance;
   let segmentsSpy: jest.SpyInstance;
@@ -25,9 +44,11 @@ describe('EventTracker', () => {
     expect(logEventSpy).toHaveBeenCalledWith('click_event', {
       event_name: eventName,
       screen_name: 'test',
+      tracking_time: FIXED_TIME,
+      my_team: 'LG',
     });
   });
-  it('EventTracker로 감싸진 컴포넌트가 터치될 경우, 이벤트가 로깅되어야 합니다.', () => {
+  it('EventTracker로 감싸진 컴포넌트가 터치될 경우, 추가 params도 함께 로깅되어야 합니다.', () => {
     const eventName = 'test_event';
     const child = <Pressable accessibilityRole="button" />;
     const wrapper = render(
@@ -40,6 +61,8 @@ describe('EventTracker', () => {
     expect(logEventSpy).toHaveBeenCalledWith('click_event', {
       event_name: eventName,
       screen_name: 'test',
+      tracking_time: FIXED_TIME,
+      my_team: 'LG',
       test: 'test',
     });
   });
